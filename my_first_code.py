a2481bc9461317e3139b316e8c31cc3702a5bb54
James <106820162+jct-80@users.noreply.github.com>
2022-06-04 18:03:55 +0100

    my_first_code

---
+++ my_first_code.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#This is my first line of code
+print("My name is James Towler")
+print("The date is the 04th June 2022")
+
+
+# In[3]:
+
+
+#Write a program to analyse real estate data
+a, b, c, d = 45000, 23400, 67000, 34600
+Print(a+b+c+d) AS totalsales
+
+
+# In[4]:
+
+
+Print(a+b+c+d) 
+
+
+# In[5]:
+
+
+Add a+b+c+d
+
+
+# In[6]:
+
+
+Add In a+b+c+d
+
+
+# In[11]:
+
+
+a = 45000
+b = 23400
+c = 67000
+d = 34600
+e = 12900
+total_sales = a + b + c + d + e
+print(total_sales)
+
+
+# In[12]:
+
+
+avg_sales = (a + b + c + d + e)/5
+print(avg_sales)
+
+
+# In[13]:
+
+
+property_c = c - 6500
+print(property_c)
+
+
+# In[ ]:
+
+
+
+
